783b31b615b797f4b1a36cfde75fa8a169f7976f
Ray Donnelly <mingw.android@gmail.com>
2017-12-03 17:31:27 +0000

    Add support for _CONDA_PYTHON_SYSCONFIGDATA_NAME if _PYTHON_SYSCONFIGDATA_NAME is not set and ignore {_CONDA,}_PYTHON_SYSCONFIGDATA_NAME if it does not exist
    This is important when bootstrapping a Miniconda (however if you are using
    a Python that pre-dates this change then you will need to apply this patch
    manually).

--- Lib/distutils/sysconfig.py
+++ Lib/distutils/sysconfig.py
@@ -433,11 +433,13 @@ def _init_posix():
     """Initialize the module as appropriate for POSIX systems."""
     # _sysconfigdata is generated at build time, see the sysconfig module
     name = os.environ.get('_PYTHON_SYSCONFIGDATA_NAME',
-        '_sysconfigdata_{abi}_{platform}_{multiarch}'.format(
-        abi=sys.abiflags,
-        platform=sys.platform,
-        multiarch=getattr(sys.implementation, '_multiarch', ''),
-    ))
+        os.environ.get('_CONDA_PYTHON_SYSCONFIGDATA_NAME',
+            '_sysconfigdata_{abi}_{platform}_{multiarch}'.format(
+            abi=sys.abiflags,
+            platform=sys.platform,
+            multiarch=getattr(sys.implementation, '_multiarch', ''))
+        )
+    )
     _temp = __import__(name, globals(), locals(), ['build_time_vars'], 0)
     build_time_vars = _temp.build_time_vars
     global _config_vars
